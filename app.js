9a94a41289a5c7b5a3db656599346967c25a2de2
knightow1 <nick.asmussen1@gmail.com>
2017-05-10 07:37:11 -0700

    added the console.log

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ alert('All questions require yes/no or y/n answers.');
 while (true) {
   var music = prompt('First question: Does Nick play Jazz?').toLowerCase();
 
+  console.log('answer: ', music);
+
   if (music === 'n' || music === 'no') {
     alert('You got it! Great job.');
     break;
@@ -18,6 +20,8 @@ while (true) {
 }
 while (true){
   var food = prompt('Second question: Is Nick\'s favorite food tacos?').toLowerCase();
+
+  console.log('answer: ', food);
 
   if(food === 'n' || food === 'no') {
     alert('Another right answer. You\'re on a roll.');
@@ -32,6 +36,8 @@ while (true){
 while (true) {
   var travel = prompt('Third question: Has Nick been to Burma?').toLowerCase();
 
+  console.log('answer: ', travel);
+
   if (travel === 'n' || travel === 'no') {
     alert('Wow you guys must be good friends.');
     break;
@@ -45,6 +51,8 @@ while (true) {
 while (true) {
   var color = prompt('Fourth question: Is Nick\'s favorite color green?').toLowerCase();
 
+  console.log('answer: ', color);
+
   if (color === 'y' || color === 'yes') {
     alert('Good job. Almost done.');
     break;
@@ -57,6 +65,8 @@ while (true) {
 }
 while (true) {
   var games = prompt('Last question: Does Nick like to play video games?').toLowerCase();
+
+  console.log('answer: ', games);
 
   if (games === 'y' || games === 'yes') {
     alert('Amazing. Thanks for playing the Nick guessing game.');
